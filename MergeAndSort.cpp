9bb4bbf9657f18a04889e10d43c08909f70e4fff
Quazi Irfan GD <quazirfan@gmail.com>
2017-01-22 15:55:29 -0600

    Add documentation to MergeAndSort.cpp

--- MergeAndSort.cpp
+++ MergeAndSort.cpp
@@ -1,3 +1,36 @@
+// Student : Quazi Irfan
+// Course : CSC-461 Programming Languages (Assignment 1)
+// Instructor : Dr. Sung Shin
+// Due Date : Jan 23rd, 2017
+// File Name : MergeAndSort.cpp
+
+// Program description :
+// This program asks the user to input two list of double numbers.
+// For every list the program asks how many numbers there should be in the list, and then it prompts the user that many time to enter those numbers.
+// Note that each number has to be entered into a separate line.
+// Then the program prints both the lists.
+// Then we check find the numbers in array1 that are not duplicated in array2. Then we push the numbers in array2.
+// Then we feed the array2 into our insertion sort to sort the elements in ascending order.
+// Finally the program terminates by printing the sorted array2.
+
+// Sample I/O:
+//Enter length of first array : 3
+//Array element 1 :1.1
+//Array element 2 :2.3
+//Array element 3 :3.4
+//Enter length of second array : 5
+//Array element 1 :2.1
+//Array element 2 :2.2
+//Array element 3 :2.3
+//Array element 4 :3.4
+//Array element 5 :3.5
+//First array [3] :1.10 2.30 3.40
+//Second array [5] :2.10 2.20 2.30 3.40 3.50
+//Merged and Sorted [6] : 1.10,2.10,2.20,2.30,3.40,3.50,
+
+// Input restriction :
+// Each list of double numbers can't contain any repeating numbers.
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -33,6 +66,16 @@ int main(){
         array2[i] = temp;
     }
 
+    // print the number lists
+    cout << "First array [" << array1Length << "] : ";
+    for(unsigned int i = 0; i<array1.size(); i++){
+        cout << array1[i] << " ";
+    }
+    cout << "\nSecond array [" << array2Length << "] : ";
+    for(unsigned int i = 0; i<array2.size(); i++){
+        cout << array2[i] << " ";
+    }
+
     // Merge and delete duplicate items
     vector<double>::iterator p = array1.begin();
     while(p != array1.end()){
@@ -53,12 +96,13 @@ int main(){
     insertionSort(array3, array2.size());
 
     // print the sorted array
-    cout << "\nMerged and Sorted : ";
+    cout << "\nMerged and Sorted [" << array2.size() << "] : ";
     for(unsigned int i = 0; i<array2.size(); i++){
         cout << array3[i] << ",";
     }
 }
 
+// insertionSort method accepts an array of double and the size of the array as argument and sorts the content of the array in ascending order.
 void insertionSort(double array[], int size){
     for(int i = 1; i<size; i++){
         int l = i;
@@ -73,20 +117,11 @@ void insertionSort(double array[], int size){
     }
 }
 
+// this is a helper method to check if a number is available in an vector
+// returns true is available, false otherwise
 bool contains(double item, vector<double> array){
     if(find(array.begin(), array.end(), item) != array.end())
         return true;
 
     return false;
-}
-
-
-void print(vector<double> array1, vector<double> array2){
-    cout << "Array 1 : ";
-    for(unsigned int i = 0; i<array1.size(); i++)
-        cout << array1[i] << " " ;
-
-    cout << "\nArray 2 : ";
-    for(unsigned int i = 0; i<array2.size(); i++)
-        cout << array2[i] << " " ;
 }
